b16f4fe3707a3cff28f760ae106b5b26ed3aeb71
martinpucik <martin.pucik@me.com>
2020-11-01 11:34:35 +0100

    fix: Getting token method promise type

--- Sources/CTKitSwift/CTKitSwift.swift
+++ Sources/CTKitSwift/CTKitSwift.swift
@@ -5,10 +5,10 @@
 //  Created by Martin Púčik on 16/03/2020.
 //
 
+import Foundation
 import Combine
 
 public enum CTKit {
-
     static func programmes() -> AnyPublisher<[CTKProgramme], Error> {
         let ddd = token().flatMap {
             API.programmelist(token: $0).execute() as AnyPublisher<[CTKProgramme], Error>
@@ -21,7 +21,7 @@ public enum CTKit {
 private extension CTKit {
     static func token() -> AnyPublisher<String, Error> {
         if let token = CTKDefaults.value(for: "token") as? String {
-            return Future<String, Error> { $0(.success(token) }.eraseToAnyPublisher()
+            return Result.Publisher(token).eraseToAnyPublisher()
         }
         let req: AnyPublisher<CTKToken, Error> = API.token.execute()
         return req.compactMap { $0.value }.eraseToAnyPublisher()
